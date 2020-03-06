1422c0b6ba105be844df5fe072ea794c4f97f581
tod619 <trevorodwyer619@hotmail.com>
2020-03-06 16:18:28 +0000

    add play pause and stop function to app.js

--- app.js
+++ app.js
@@ -3,3 +3,51 @@ const play = document.getElementById("play");
 const stop = document.getElementById("stop");
 const progress = document.getElementById("progress");
 const timestamp = document.getElementById("timestamp");
+
+// Functions
+
+// Play & Pause videos
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// Update play pause icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class = "fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class = "fa fa-pause fa-2x"></i>';
+  }
+}
+
+// Update progress and timestamp
+function updateProgress() {
+  return true;
+}
+
+// set video time to progress
+function setVideoProgress() {
+  return true;
+}
+
+// Stop Video
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
+
+//Event Listners
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+
+stop.addEventListener("click", stopVideo);
+
+progress.addEventListener("change", setVideoProgress);
